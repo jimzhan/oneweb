147af476511d67a4cb18f032ef04d07d01d5de61
Jim Zhan <jim_zhan@epam.com>
2024-07-31 21:22:33 +0800

    add basic CRUD for knex

--- backoffice/src/db/db.js
+++ backoffice/src/db/db.js
@@ -1,6 +1,7 @@
 import Knex from 'knex'
 import config from 'config'
 import { ObjectId } from 'bson'
+import assert from 'node:assert'
 
 const db = Knex(config.db)
 
@@ -8,7 +9,28 @@ export default db
 
 export const Key = () => new ObjectId().toHexString()
 
-export const insert = async (table, data = {}) => {
+const ensureId = (id) => assert(ObjectId.isValid(id), 'id is not valid')
+
+export const insert = async (table, data = {}, tx = null) => {
   const id = Key()
-  return await db(table).insert(Object.assign({}, data, { id }))
+  const Q = tx ? db(table).transacting(tx) : db(table)
+  return await Q.insert(Object.assign({}, data, { id }))
+}
+
+export const get = async (table, id, tx = null) => {
+  ensureId(id)
+  const Q = tx ? db(table).transacting(tx) : db(table)
+  return await Q.where({ id }).first()
+}
+
+export const update = async (table, id, data = {}, tx = null) => {
+  ensureId(id)
+  const Q = tx ? db(table).transacting(tx) : db(table)
+  return await Q.where({ id }).first().update(data)
+}
+
+export const remove = async (table, id, tx = null) => {
+  ensureId(id)
+  const Q = tx ? db(table).transacting(tx) : db(table)
+  return await Q.where({ id }).first().del()
 }
