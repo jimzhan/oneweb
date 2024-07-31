1eb80b281ecd888e91400c04d1441c85478d206c
Jim Zhan <jim_zhan@epam.com>
2024-07-31 12:54:06 +0800

    add schema test cases

---
+++ backoffice/src/apps/auth/auth.schema.spec.js
@@ -0,0 +1,21 @@
+import * as schema from './auth.schema.js'
+import { describe, expect, it } from 'vitest'
+
+
+describe('Auth Schema', () => {
+  it('should pass & return a valid login', async () => {
+    const payload = { username: 'test', password: 'test' }
+    let { error, value } = schema.login.validate(payload)
+    expect(error).toBeTruthy()
+    expect(value).toEqual(payload)
+  })
+
+  it('should fail an invalid login', async () => {
+    const payload = { username: 'test@test.com', password: 'test#fdfd67&' }
+    let { error, value } = schema.login.validate(payload)
+    expect(error).toBeUndefined()
+    expect(value).toEqual(payload)
+  })
+})
+
+
