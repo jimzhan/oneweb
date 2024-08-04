4b9abd3bc66978961527c5c3e99e0140646c9467
jimzhan <jim_zhan@epam.com>
2024-08-04 11:29:29 +0800

    enable cors for production mode

--- backoffice/db/seeds/01.users.js
+++ backoffice/db/seeds/01.users.js
@@ -1,11 +1,11 @@
-import { Key } from '../../src/db/index.js'
 import { encrypt } from '../../src/core/secret.js'
 
 const tableName = 'users'
 const password = await encrypt('password')
 
 export const seed = async (knex) => {
+  // **NOTE** MSSQL does not support `onConflict` , use `knex.raw` instead.
   await knex(tableName).insert([
-    { id: Key(), username: 'dev@example.com', password }
-  ])
+    { id: '66aef3aa171c65695403843d', username: 'dev@example.com', password }
+  ]).onConflict('username').ignore()
 }
