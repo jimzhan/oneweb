3ed209a90933e5da7fb7546bcf6f84f3578d7a4e
jimzhan <jim.zhan@me.com>
2025-03-07 17:57:21 +0800

    enable module alias & confidence

---
+++ settings/index.js
@@ -0,0 +1,24 @@
+import { Record } from 'immutable'
+import { Store } from '@hapipal/confidence'
+import server from './server.js'
+// ----------------------------------------------------------------------
+//  Environment based filters:
+//    - `process.env.NODE_ENV`: development | test | production
+//    - `process.env.STAGE`: ci | dev | sit | uat | prod.
+// ----------------------------------------------------------------------
+const { env } = process
+
+const internals = {
+  criteria: {
+    env: env.NODE_ENV,
+    stage: env.STAGE
+  }
+}
+
+internals.settings = {
+  $meta: 'application settings file',
+  server
+}
+
+internals.store = new Store(internals.settings)
+export default Record(internals.store.get('/', internals.criteria))()
